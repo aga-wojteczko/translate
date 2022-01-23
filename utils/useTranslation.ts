0e7355516a174ab97e05892672fd7b978a8c04b4
burakukula <trycjapa@gmail.com>
2022-01-23 12:47:40 +0100

    fix: fix ts error

--- utils/useTranslation.ts
+++ utils/useTranslation.ts
@@ -1,3 +1,5 @@
 // This is where you could move the fetch funtionality
 // Maybe you could consider using react hook for that purpuse
 // Read more https://reactjs.org/docs/hooks-custom.html
+
+export {}
