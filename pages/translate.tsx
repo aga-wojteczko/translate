04fbc5b091f4fdcfff201e922d39a7cf7e4cf8d3
aga-wojteczko <agnieszkadudek9@gmail.com>
2022-01-25 20:53:36 +0100

    moved translation logic to utils/useTranslation.ts and started to createa custom hook. Still not used

--- pages/translate.tsx
+++ pages/translate.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { TextInput } from '../components/TextInput';
+// import React, { useEffect, useState } from 'react';
+// import { TextInput } from '../components/TextInput';
 // you could move the fetching functionality to separate custom hook
-/// import { useTranslation } from '../utils/useTranslation';
+import { useTranslation } from '../utils/useTranslation';
 
 export default function Translate() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
+  // const [data, setData] = useState(null)
+  // const [isLoading, setLoading] = useState(false)
   
   const text = 'Hello';
   const targetLang = 'PL';
-  const apiKey = process.env.API_KEY;
+  // const apiKey = process.env.API_KEY;
 
-  useEffect(() => {
-    setLoading(true)
-    fetch(`https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${text}&target_lang=${targetLang}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
+  // useEffect(() => {
+  //   setLoading(true)
+  //   fetch(`https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${text}&target_lang=${targetLang}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data)
+  //       setLoading(false)
+  //     })
+  // }, [])
 
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No data</p>
+  // if (isLoading) return <p>Loading...</p>
+  // if (!data) return <p>No data</p>
 
   console.log('data:', data)
   return (
--- utils/useTranslation.ts
+++ utils/useTranslation.ts
@@ -1,5 +1,28 @@
 // This is where you could move the fetch funtionality
 // Maybe you could consider using react hook for that purpuse
 // Read more https://reactjs.org/docs/hooks-custom.html
+import React, { useEffect, useState } from 'react';
+import { TextInput } from '../components/TextInput';
 
-export {}
+
+export default function useTranslation(text,targetLang) => {
+    return(
+        const [data, setData] = useState(null)
+        const [isLoading, setLoading] = useState(false)
+        const apiKey = process.env.API_KEY
+
+        useEffect(() => {
+            setLoading(true)
+            fetch(`https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${text}&target_lang=${targetLang}`)
+              .then((res) => res.json())
+              .then((data) => {
+                setData(data)
+                setLoading(false)
+              })
+          }, [])
+
+        if (isLoading) return <p>Loading...</p>
+         if (!data) return <p>No data</p>
+
+    )
+}
