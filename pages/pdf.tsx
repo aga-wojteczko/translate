776f2b00a64163722e34e1113e454a3b0b542afd
aga-wojteczko <agnieszkadudek9@gmail.com>
2022-01-26 14:55:15 +0100

    used pdf webviewer, but having some issues import. module not found

--- pages/pdf.tsx
+++ pages/pdf.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import WebViewer from '@pdftron\webviewer';
+import WebViewer from '@pdftron\webviewer'; //module not found
 
-// need to use PDFviewer - already installed
 
 export default function ViewPDF () {
   useEffect(()=> {
-    WebViever({path:'lib' initialDoc: '..\.next\static\pdfs\PAC 30 02.pdf'}
+    WebViewer({path:'lib' , initialDoc: '..\.next\static\pdfs\PAC 30 02.pdf' }, viewerDiv.current as HTMLElement).then(instance => {
+
+    });
   }, [])
     const viewerDiv = useRef<HTMLDivElement>(null);
     return (
