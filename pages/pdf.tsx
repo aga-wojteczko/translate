e1a81aa1953ad73fa2e991b6865bfec2482b9c4d
aga-wojteczko <agnieszkadudek9@gmail.com>
2022-01-26 19:08:52 +0100

    problem with finding pdftron webviewer module solved

--- pages/pdf.tsx
+++ pages/pdf.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useRef } from 'react';
-import WebViewer from '@pdftron\webviewer'; //module not found
+import WebViewer from '@pdftron/webviewer'; //module not found
 
 
 export default function ViewPDF () {
   useEffect(()=> {
-    WebViewer({path:'lib' , initialDoc: '..\.next\static\pdfs\PAC 30 02.pdf' }, viewerDiv.current as HTMLElement).then(instance => {
+    WebViewer({path:'lib' , initialDoc: '../.next/static/pdfs/PAC 30 02.pdf' }, viewerDiv.current as HTMLElement).then(instance => {
 
     });
   }, [])
     const viewerDiv = useRef<HTMLDivElement>(null);
     return (
-      <div className = 'webviewer' ref = {viewerDiv}>
+      <div className='webviewer' ref={viewerDiv}>
     
       </div> 
     )
