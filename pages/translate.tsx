aa16335e738398aef448fe4019f49456664a5583
aga-wojteczko <agnieszkadudek9@gmail.com>
2022-02-18 09:37:45 +0100

    changed useEffect hook with fetch to click handler. Returns no data

--- pages/translate.tsx
+++ pages/translate.tsx
@@ -11,22 +11,28 @@ export default function Translate() {
   const targetLang = 'PL'; //input data, we can change
   // const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-  useEffect(() => {
-    setLoading(true)
-    fetch(`https://api-free.deepl.com/v2/translate?auth_key=9dee5181-f767-85aa-06e6-00293e8adde7:fx&text=${text}&target_lang=${targetLang}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
+  // useEffect(() => {
+  //   setLoading(true)
+  //   fetch(`https://api-free.deepl.com/v2/translate?auth_key=9dee5181-f767-85aa-06e6-00293e8adde7:fx&text=${text}&target_lang=${targetLang}`)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data)
+  //       setLoading(false)
+  //     })
+  // }, [])
 
   if (isLoading) return <p>Loading...</p>
   if (!data) return <p>No data</p>
 
   console.log('data:', data)
   const handleClick = () => {
-    //here we should have fetch
+    setLoading(true)
+      fetch(`https://api-free.deepl.com/v2/translate?auth_key=9dee5181-f767-85aa-06e6-00293e8adde7:fx&text=${text}&target_lang=${targetLang}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data)
+          setLoading(false)
+        })
     console.log('click', text)
   }
   return (
